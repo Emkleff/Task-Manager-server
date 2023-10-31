de74018a90659b09cd136c24b09c00aa5d023736
Emkleff <nwodoemekamoses@gmail.com>
2023-10-31 15:48:40 +0100

    feat: implemented cors

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require('cors')
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
-const taskRouter = require('./Routes/taskRouter')
+const taskRouter = require("./Routes/taskRouter");
 //MIDDLE-WARES
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 //routes
-app.use( '/api/task', taskRouter)
-
-
+app.use("/api/task", taskRouter);
 
 const startServer = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-        dbName: 'goals',
+      dbName: "goals",
     });
     app.listen(PORT, () => {
       console.log(`server running on port : ${PORT}..`);
